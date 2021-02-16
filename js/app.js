2b80e85cc0ded73a3d311f13ec7f07668a248986
Muhanad-nabeel <mohannadabdulwahid@yahoo.com>
2021-02-16 06:04:49 -0600

    add fun

--- js/app.js
+++ js/app.js
@@ -16,13 +16,12 @@ for (let i = 0; i < timeArr.length; i++) {
   headCellEl.textContent = timeArr[i];
 }
 function Salmon(location, min, max, avgCookie, cookiePurArray, total) {
+this.location=location;
+this.min=min;
+this.avgCookie=avgCookie;
+this.cookiePurArray=cookiePurArray;
+this.total=total;
 
-this.location = location;
-  this.min = min;
-  this.max = max;
-  this.avgCookie = avgCookie;
-  this.cookiePurArray = cookiePurArray;
-  this.total = total;
   Salmon.prototype.getCookiePerCust = function () {
     for (let i = 0; i < (timeArr.length - 1); i++) {
       this.cookiePurArray[i] = Math.floor(getRandomNo(this.min, this.max) * this.avgCookie);
